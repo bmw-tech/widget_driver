372cd398286713be28ca95e08ecef96903109bce
Jan Peter <34645936+JanMichaelPeter@users.noreply.github.com>
2022-12-16 10:33:12 +0100

    create a one for all publishing job to pub.dev

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,7 +5,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 TAG_COLOR="\n${GREEN}${TAG}${NC}"
-PROJECT_FOLDERS=("widget_driver" "widget_driver_generator" "widget_driver_test")
+PROJECT_FOLDERS=("widget_driver" "widget_driver_annotation" "widget_driver_generator" "widget_driver_test")
 
 function run_tests() {
     current_dir=$(pwd)
