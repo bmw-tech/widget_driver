0b4504355a85fc86d2a2fbbae72e0f97cdbec2b6
Fabian Rother <fabian.rother@bmw.de>
2023-02-06 15:06:09 +0100

    docs for annotatedElement

--- widget_driver_generator/lib/src/models/annotated_element.dart
+++ widget_driver_generator/lib/src/models/annotated_element.dart
@@ -1,6 +1,8 @@
 import 'package:analyzer/dart/element/element.dart';
 import 'package:widget_driver_generator/src/utils/element_utils.dart';
 
+/// Class representing fields, properties and methods that have been annotated with either `TestDriverDefaultValue` or
+/// `TestDriverDefaultFutureValue`, to keep track of the [returnValue] and [codeDefinition].
 class AnnotatedElement {
   final String returnValue;
   final String codeDefinition;
