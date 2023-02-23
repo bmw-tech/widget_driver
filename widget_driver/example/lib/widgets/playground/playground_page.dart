e0ce7c0ed611e3a812381486328ba5d45b0492c6
Robert Magnusson <59053438+robert-northmind@users.noreply.github.com>
2023-02-23 11:34:14 +0100

    Adds a new tab for playground code. Where you can quickly add some test code (#105)
    * Adds a new tab for playground code. Where you can quickly add some test code
    
    * Fixes analyzer issue

---
+++ widget_driver/example/lib/widgets/playground/playground_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlaygroundPage extends StatelessWidget {
+  const PlaygroundPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Put your fun playground test code here'));
+  }
+}
