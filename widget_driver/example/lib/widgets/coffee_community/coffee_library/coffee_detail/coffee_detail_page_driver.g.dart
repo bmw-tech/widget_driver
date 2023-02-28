86016f558701647fc9cd12e0f5405ac4cddbdef3
Fabian Rother <fabian.rother@t-online.de>
2023-02-28 16:33:47 +0100

    example changes and widgetDriver version bump

--- widget_driver/example/lib/widgets/coffee_community/coffee_library/coffee_detail/coffee_detail_page_driver.g.dart
+++ widget_driver/example/lib/widgets/coffee_community/coffee_library/coffee_detail/coffee_detail_page_driver.g.dart
@@ -44,4 +44,42 @@ class $CoffeeDetailPageDriverProvider extends WidgetDriverProvider<CoffeeDetailP
   CoffeeDetailPageDriver buildTestDriver() {
     return _$TestCoffeeDetailPageDriver();
   }
+
+  @override
+  void updateDriverProvidedProperties(CoffeeDetailPageDriver driver) {
+    //  In case you get a compiler error here, you have to mixin _$DriverProvidedPropertiesMixin into your driver.
+    //  And implement updateDriverProvidedProperties(), there you can react to new values to all your provided values.
+    //  Like this:
+    //  class CoffeeDetailPageDriver extends WidgetDriver with _$DriverProvidedPropertiesMixin {
+    //
+    //    ...
+    //
+    //    @override
+    //    void updateDriverProvidedProperties(...) {
+    //      // Handle your updates
+    //    }
+    //  }
+    driver.updateDriverProvidedProperties(
+      newIndex: _index,
+      newCoffee: _coffee,
+    );
+  }
+}
+
+mixin _$DriverProvidedPropertiesMixin {
+  /// This function allows you to react to state changes in the driver.
+  /// It provides you with the new values to the properties that you passed into the driver.
+  /// This is because the driver does not get recreated on state changes.
+  /// Important, you do not need to call `notifyWidget()` in this function.
+  /// It gets called in the build method of the widget, slightly before rendering.
+  /// Thus all data changed here will be shown with the "currently ongoing state change".
+  ///
+  /// Very Important!!
+  /// Because this function is running during the build process,
+  /// it is NOT the place to run time cosuming or blocking tasks etc. (like calling Api-Endpoints)
+  /// This could greatly impact your apps performance.
+  void updateDriverProvidedProperties({
+    required int newIndex,
+    required Coffee newCoffee,
+  });
 }
