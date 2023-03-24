4dfffa856d79b4e6522179cec2413585980a985a
Robert Magnusson <robert@northmind.io>
2023-03-24 14:51:47 +0100

    Adds comment to one part of example code

--- widget_driver/example/lib/widgets/coffee_community/coffee_community_page_driver.dart
+++ widget_driver/example/lib/widgets/coffee_community/coffee_community_page_driver.dart
@@ -14,6 +14,18 @@ class CoffeeCommunityPageDriver extends WidgetDriver {
   @override
   void didUpdateBuildContext(BuildContext context) {
     _authService = context.read<AuthService>();
+
+    // If you would have grabbed something here using `context.watch`
+    // then the `didUpdateBuildContext` method could get called more times.
+    // In these cases you need to clean up previous state.
+    // E.g. by calling `cancel()` on a previous subscription.
+    //
+    // _subscription?.cancel();
+    //
+    // But here we do not need to do this since we are only reading the dependency
+    // out of the context. So in this case, we know that the
+    // `didUpdateBuildContext` is only called once.
+
     _subscription = _authService.isLoggedInStream.listen((_) {
       notifyWidget();
     });
