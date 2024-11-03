43039a5f1d8f518b8ac75ca5ee8907d9cc4e6ef6
Breno Battaglin <8771994+brenobattaglin@users.noreply.github.com>
2024-11-02 21:05:53 -0300

    test: create test group for widget tester extension

--- widget_driver_test/test/widget_tester_extension_test.dart
+++ widget_driver_test/test/widget_tester_extension_test.dart
@@ -7,26 +7,28 @@ import 'package:widget_driver_test/widget_driver_test.dart';
 class MockWidgetDriver extends Mock implements WidgetDriver {}
 
 void main() {
-  testWidgets('should return DriverTester', (WidgetTester tester) async {
-    final driverTester = await tester.getDriverTester<MockWidgetDriver>(
-      driverBuilder: () => MockWidgetDriver(),
-    );
+  group('WidgetTesterExtension:', () {
+    testWidgets('Should return DriverTester', (WidgetTester tester) async {
+      final driverTester = await tester.getDriverTester<MockWidgetDriver>(
+        driverBuilder: () => MockWidgetDriver(),
+      );
 
-    expect(driverTester, isNotNull);
-    expect(driverTester.driver, isA<MockWidgetDriver>());
-  });
+      expect(driverTester, isNotNull);
+      expect(driverTester.driver, isA<MockWidgetDriver>());
+    });
 
-  testWidgets('should work with parentWidgetBuilder', (WidgetTester tester) async {
-    final driverTester = await tester.getDriverTester<MockWidgetDriver>(
-      driverBuilder: () => MockWidgetDriver(),
-      parentWidgetBuilder: (driverWidget) {
-        return MaterialApp(
-          home: Scaffold(body: driverWidget),
-        );
-      },
-    );
+    testWidgets('Should work with parentWidgetBuilder', (WidgetTester tester) async {
+      final driverTester = await tester.getDriverTester<MockWidgetDriver>(
+        driverBuilder: () => MockWidgetDriver(),
+        parentWidgetBuilder: (driverWidget) {
+          return MaterialApp(
+            home: Scaffold(body: driverWidget),
+          );
+        },
+      );
 
-    expect(driverTester, isNotNull);
-    expect(driverTester.driver, isA<MockWidgetDriver>());
+      expect(driverTester, isNotNull);
+      expect(driverTester.driver, isA<MockWidgetDriver>());
+    });
   });
 }
