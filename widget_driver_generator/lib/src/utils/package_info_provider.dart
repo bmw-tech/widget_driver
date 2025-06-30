a8f806f574a67c742a06007b3a697b535c18aa55
Andre Toropchin <andre.toropchin@bmw.de>
2025-06-30 11:55:44 +0200

    Fixes bug with code generation in child packages using dart 3.6.2

--- widget_driver_generator/lib/src/utils/package_info_provider.dart
+++ widget_driver_generator/lib/src/utils/package_info_provider.dart
@@ -10,7 +10,19 @@ class PackageInfoProvider {
 
   /// Grabs the version string for widget_driver_generator from the pubspec.lock
   Future<String> getPackageVersionString() async {
-    final pubspecLockString = await _fileReader.readFile('pubspec.lock');
+    late String pubspecLockString;
+    Directory workspaceDir = Directory('.').absolute;
+    File pubspecFile = File('${workspaceDir.path}/pubspec.yaml');
+
+    if (_hasWorkspace(pubspecFile)) {
+      while (!_isWorkSpaceRoot(pubspecFile)) {
+        workspaceDir = workspaceDir.parent.absolute;
+        pubspecFile = File('${workspaceDir.path}/pubspec.yaml');
+      }
+      pubspecLockString = await _fileReader.readFile('${workspaceDir.path}/pubspec.lock');
+    } else {
+      pubspecLockString = await _fileReader.readFile('pubspec.lock');
+    }
 
     final packageStartIndex = pubspecLockString.indexOf('widget_driver_generator:');
     if (packageStartIndex < 0) {
@@ -25,6 +37,22 @@ class PackageInfoProvider {
     final stringToCheck = pubspecLockString.substring(endOfVersionIndex);
     final versionNumberString = const LineSplitter().convert(stringToCheck).first;
     return versionNumberString;
+  }
+
+  bool _isWorkSpaceRoot(File pubspec) {
+    if (!pubspec.existsSync()) {
+      return false;
+    }
+    return pubspec.readAsLinesSync().where((e) => e.startsWith('workspace:')).isNotEmpty;
+  }
+
+  bool _hasWorkspace(File pubspec) {
+    return pubspec
+        .readAsLinesSync()
+        .where(
+          (e) => e.startsWith('resolution:'),
+        )
+        .isNotEmpty;
   }
 }
 
